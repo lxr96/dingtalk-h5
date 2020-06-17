cfb98d848f2c9b9e3911412636b94e192bacd536
LXR <LXR@LAPTOP-0KIAIGL4.mshome.net>
2020-06-17 11:11:42 +0800

    test

--- backend/src/main/java/com/dingtalk/h5app/quickstart/util/Test.java
+++ backend/src/main/java/com/dingtalk/h5app/quickstart/util/Test.java
@@ -6,14 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 
 	}
-	/*
 	public void test() {
-		DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/topapi/smartwork/hrm/employee/list");
-		OapiSmartworkHrmEmployeeListRequest req = new OapiSmartworkHrmEmployeeListRequest();
-		req.setUseridList("123, 234");
-		req.setFieldFilterList("sys00-name,sys00-email");
-		OapiSmartworkHrmEmployeeListResponse rsp = client.execute(req, access_token);
-		System.out.println(rsp.getBody());
-	}*/
+
+	}
 
 }
